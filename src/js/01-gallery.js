917425a2daba9d935933b5e1d19e3e873d891c3c
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-15 18:54:37 +0300

    added js from the previous hw+added video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item"> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a> </li>`;
+    })
+    .join(' ');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
